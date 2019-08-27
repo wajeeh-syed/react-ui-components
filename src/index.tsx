4ac7b1e8d1b8c6c2ddba5582899254f3380a44e5
Hafeez Syed <hafeez.syed@nintex.com>
2019-08-27 10:49:55 +1000

    readme updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import * as React from 'react';
 import { Checkbox } from './Checkbox';
 import { InputField } from './InputField';
 
 const main = (): string => `Welcome to React UI Components world`;
-
 
 export { main, Checkbox, InputField };
